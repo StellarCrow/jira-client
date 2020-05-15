a7d5cddd5efa88cb66cd999e7b2374428fc427d1
StellarCrow <dariadar12@gmail.com>
2020-05-15 12:31:38 +0300

    add components for modal

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -6,10 +6,13 @@ import { MainPageComponent } from './components/pages/main-page/main-page.compon
 import { SharedModule } from '../../shared/shared.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { BoardComponent } from './components/board/board.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { AddTaskModalComponent } from './components/add-task-modal/add-task-modal.component';
 
 
 @NgModule({
-  declarations: [MainPageComponent, ToolbarComponent, BoardComponent],
+  declarations: [MainPageComponent, ToolbarComponent, BoardComponent, TaskFormComponent, ModalComponent, AddTaskModalComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
